50344f9dd959e9771c80c12ebdb2b8ceb9df0b94
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2023-12-30 12:12:00 +0000

    Changed to use tailwind and added a navbar and footer

--- be-nice-band/src/App.js
+++ be-nice-band/src/App.js
@@ -1,53 +1,14 @@
-import React from 'react';
-import './App.css';
+import './index.css';
+import Navbar from './Navigation/Navbar.js'
+import Footer from './Footer/Footer.js'
 
-const user = {
-  name: 'Alex McIntosh',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
-
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
-
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
-
-export default function MyApp() {
+function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-      <AboutPage />
-      <Profile />
+      <Navbar />
+      <Footer />
     </div>
   );
 }
+
+export default App;
