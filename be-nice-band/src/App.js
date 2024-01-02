3792934f45c3f0ef887cedab6aa0ba5f95545035
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-01-02 20:53:43 +0000

    Added a carousel and some gradient to the site

--- be-nice-band/src/App.js
+++ be-nice-band/src/App.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import './index.css';
-import Navbar from './Navigation/Navbar.js'
-import Section from './Section/Section.js'
-import EPK from './EPK/EPK.js';
-import LiveDatesSection from './LiveDatesSection/LiveDatesSection.js'
-import Footer from './Footer/Footer.js'
-
+import React from "react";
+import "./index.css";
+import Navbar from "./Navigation/Navbar.js";
+import Section from "./Section/Section.js";
+import EPK from "./EPK/EPK.js";
+import LiveDatesSection from "./LiveDatesSection/LiveDatesSection.js";
+import Footer from "./Footer/Footer.js";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div class="h-screen" style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL + '/BackgroundImages/indexphoto.jpg'})`,
-        backgroundSize: 'cover',
-      }}>
+
+      <div
+        class="h-screen"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + "/BackgroundImages/indexphoto.jpg"
+          })`,
+          backgroundSize: "cover",
+        }}
+      >
         <Section />
       </div>
-      
 
-      <div class="h-screen" style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL + 'BackgroundImages/space.gif'})`,
-        backgroundSize: 'cover',
-      }}>
+      <div class="section-border mx-auto h-screen bg-gradient-to-r via-purple-500 from-indigo-500 to-blue-500">
         <EPK />
       </div>
-      
 
-
-      <div class="h-screen" style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL + 'BackgroundImages/space.gif'})`,
-        backgroundSize: 'cover',
-      }}>
+      <div
+        class="h-screen"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + "BackgroundImages/space.gif"
+          })`,
+          backgroundSize: "cover",
+        }}
+      >
         <LiveDatesSection />
       </div>
-      
+
       <Footer />
     </div>
   );
--- be-nice-band/src/Section/Section.js
+++ be-nice-band/src/Section/Section.js
@@ -1,8 +1,7 @@
 export default function Section() {
   return (
     <div class="section-border mx-auto h-screen">
-      <div class="container mx-auto">
-      </div>
+      <div class="container mx-auto"/>
     </div>
   );
 }
