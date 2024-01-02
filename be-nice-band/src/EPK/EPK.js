f397f9b64c07318a0759d048367dda443cb9552f
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-01-02 19:57:52 +0000

    added epk page

---
+++ be-nice-band/src/EPK/EPK.js
@@ -0,0 +1,9 @@
+export default function EPK() {
+    return (
+      <div class="section-border mx-auto h-screen">
+        <div class="container mx-auto">
+        </div>
+      </div>
+    );
+  }
+  
