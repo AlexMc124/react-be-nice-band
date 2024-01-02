1bfe64b4cae6f22e3ac84fd8a2c649d910e01d5e
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-01-02 19:57:40 +0000

    Removed image folder and added epk page

--- be-nice-band/src/App.js
+++ be-nice-band/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import Navbar from './Navigation/Navbar.js'
 import Section from './Section/Section.js'
+import EPK from './EPK/EPK.js';
 import LiveDatesSection from './LiveDatesSection/LiveDatesSection.js'
 import Footer from './Footer/Footer.js'
 
@@ -17,12 +18,23 @@ function App() {
         <Section />
       </div>
       
+
+      <div class="h-screen" style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL + 'BackgroundImages/space.gif'})`,
+        backgroundSize: 'cover',
+      }}>
+        <EPK />
+      </div>
+      
+
+
       <div class="h-screen" style={{
         backgroundImage: `url(${process.env.PUBLIC_URL + 'BackgroundImages/space.gif'})`,
         backgroundSize: 'cover',
       }}>
         <LiveDatesSection />
       </div>
+      
       <Footer />
     </div>
   );
--- be-nice-band/src/LiveDatesSection/LiveDatesSection.js
+++ be-nice-band/src/LiveDatesSection/LiveDatesSection.js
@@ -23,17 +23,12 @@ const live_dates = [
 ];
 
 
-// GIGS: 
-// 09/01 The Horn, St Albans 
-// 13/01 Poco Loco, Chatham
-// 02/02 Three Wise Monkeys, Colchester
-
 
 export default function LiveDatesSection() {
   return (
-    <div className="live-dates-section" class="container mx-auto p-10 ">
+    <div className="live-dates-section" class="container mx-auto p-10 text-center mb-9">
       {/* make something that generates a list of gigs based on each of the entries in the live_dates obj */}
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white centre">
+      <h1 className="mb-10 text-4xl font-extrabold text-gray-900 align-centre md:text-5xl lg:text-6xl dark:text-white mx-auto">
         Live Dates
       </h1>
       {live_dates.map((live_date) => (
@@ -43,9 +38,9 @@ export default function LiveDatesSection() {
         >
           <div
             className="live-dates-section__gig"
-            class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+            class="mb-9 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white"
           >
-            <div>{live_date.location.toUpperCase()} - {live_date.venue.toUpperCase()} - {live_date.date.toUpperCase()} - <a class="bg-sky-500 hover:bg-sky-700" href={live_date.tickets}>TICKETS</a> </div>
+            <h3>{live_date.location.toUpperCase()} - {live_date.venue.toUpperCase()} - {live_date.date.toUpperCase()} - <a class="bg-sky-500 hover:bg-sky-700" href={live_date.tickets}>TICKETS</a></h3>
             <div>{live_date.description} </div>
           </div>
         </section>
