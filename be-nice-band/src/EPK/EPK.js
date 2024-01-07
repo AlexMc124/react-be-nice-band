fda3e6239d597a2b85d722a019e882b984f5535d
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-01-07 16:36:07 +0000

    Changed width and padding for epk

--- be-nice-band/src/EPK/EPK.js
+++ be-nice-band/src/EPK/EPK.js
@@ -4,7 +4,7 @@ import "yet-another-react-lightbox/styles.css";
 
 export default function EPK() {
   return (
-    <div class="bg-gradient-to-r via-purple-500 from-indigo-500 to-blue-500 p-2">
+    <div class="bg-gradient-to-r via-purple-500 from-indigo-500 to-blue-500 p-2 w-full">
       <div class="container mx-auto">
         <div class="flex flex-col items-center justify-center">
           <div class="flex flex-row items-center justify-center">
@@ -14,7 +14,7 @@ export default function EPK() {
               alt="Flaming Text"
             />
             <img
-              class="p-3 sm:w-1/2"
+              class="sm:w-1/2"
               src="Titles/flaminglogo.gif"
               alt="Flaming Text"
             />
