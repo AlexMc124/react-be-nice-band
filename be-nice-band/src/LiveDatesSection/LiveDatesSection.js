97e41b25f3e340d474f16e1f1dd878b7ff0f4891
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-01-07 18:27:45 +0000

    Added back class name and added classname to the tailwind config

--- be-nice-band/src/LiveDatesSection/LiveDatesSection.js
+++ be-nice-band/src/LiveDatesSection/LiveDatesSection.js
@@ -1,28 +1,3 @@
-function SongKick() {
-  return (
-    <a
-      href="https://www.songkick.com/artists/10250591"
-      className="songkick-widget"
-      data-theme="dark"
-      data-track-button="on"
-      data-detect-style="off"
-      data-background-color="rgb(0,0,0,1)"
-      data-font-color="rgb(255,255,255,1)"
-      data-button-bg-color="rgb(255,255,255,1)"
-      data-button-text-color="rgb(0,0,0,1)"
-      data-locale="en"
-      data-other-artists="on"
-      data-share-button="on"
-      data-country-filter="on"
-      data-rsvp="on"
-      data-request-show="on"
-      data-past-events="on"
-      data-past-events-offtour="off"
-      data-remind-me="on"
-    ></a>
-  );
-}
-
 export default function LiveDatesSection() {
   return (
     <div class="h-screen bg-center bg-space-background bg-cover">
@@ -35,7 +10,26 @@ export default function LiveDatesSection() {
         </h1>
       </div>
       <div class="container mx-auto p-10 text-center mb-9">
-        <SongKick />
+      <a
+        href="https://www.songkick.com/artists/10250591"
+        class="songkick-widget"
+        data-theme="dark"
+        data-track-button="on"
+        data-detect-style="off"
+        data-background-color="rgb(0,0,0,1)"
+        data-font-color="rgb(255,255,255,1)"
+        data-button-bg-color="rgb(255,255,255,1)"
+        data-button-text-color="rgb(0,0,0,1)"
+        data-locale="en"
+        data-other-artists="on"
+        data-share-button="on"
+        data-country-filter="on"
+        data-rsvp="on"
+        data-request-show="on"
+        data-past-events="on"
+        data-past-events-offtour="off"
+        data-remind-me="on"
+        />
       </div>
     </div>
   );
--- be-nice-band/tailwind.config.js
+++ be-nice-band/tailwind.config.js
@@ -16,4 +16,7 @@ module.exports = withMT({
     }},
   },
   plugins: [],
+  safelist: [
+    "songkick-widget"
+  ]
 });
