f199038cec09472360c578eb6bb4b1f5b6b9d9f0
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2024-04-09 14:30:17 +0200

    Added text line to the contact form

--- be-nice-band/src/pages/contact.js
+++ be-nice-band/src/pages/contact.js
@@ -17,6 +17,12 @@ function Contact() {
   return (
     <div class="h-screen bg-cover bg-centre bg-[url('BackgroundImages/backgroundimage.JPG')] h-full ">
       <div class="container pt-10">
+        <div class="container mx-auto bg-white text-black rounded">
+          For bookings, press or enquiries, get in touch via our email
+          be.nice.band1@gmail.com, or through the contact form below.
+        </div>
+      </div>
+      <div class="container pt-10">
         <ContactForm />
       </div>
     </div>
